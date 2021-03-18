fa058801fde6bd810b09aaf15c41c3dbb856a064
Julio Machado <juliocarlos00@hotmail.com>
2021-03-17 22:04:22 -0300

    add/ miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
 export function TransactionTable() {
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/transactions')
+            .then(response => response.json())
+            .then(data => console.log(data))
+    }, [])
+
     return (
         <Container>
 
@@ -17,13 +25,13 @@ export function TransactionTable() {
                 <tbody>
                     <tr>
                         <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$ 12.000</td>
+                        <td className="deposit">R$12.000</td>
                         <td>Desenvolvimento</td>
                         <td>20/02/2021</td>
                     </tr>
                     <tr>
                         <td>Aluguel</td>
-                        <td className="withdraw">- R$ 1.100</td>
+                        <td className="withdraw">- R$1.100</td>
                         <td>Casa</td>
                         <td>17/02/2021</td>
                     </tr>
